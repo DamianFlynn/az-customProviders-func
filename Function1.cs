09ed2bf01a501e0266f00a040e2376e84d0c6f58
Damian Flynn <info@damianflynn.com>
2024-02-07 11:21:44 +0000

    chore: refactor function
    rename class
    set function to async
    try catch on azure call

--- Function1.cs
+++ Function1.cs
@@ -1,24 +1,43 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using myCustomResource.services;
+using System.Net;
 
 namespace myCustomResource
 {
-    public class Function1
+    public class Actions
     {
-        private readonly ILogger<Function1> _logger;
+        private readonly ILogger<Actions> _logger;
+        private readonly AzureManagement _azureManagement;
 
-        public Function1(ILogger<Function1> logger)
+        public Actions(ILogger<Actions> logger, AzureManagement azureManagement)
         {
             _logger = logger;
+            _azureManagement  = azureManagement;
         }
 
-        [Function("Function1")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        [Function("Restart")]
+        public async Task<HttpResponseData> Restart([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+    
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+          _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+          try
+          {
+            var webApp1Id = System.Environment.GetEnvironmentVariable("WEBAPP1_ID");
+            var webApp2Id = System.Environment.GetEnvironmentVariable("WEBAPP2_ID");
+            await _azureManagement.RestartAppService(webApp1Id);
+            await _azureManagement.RestartAppService(webApp2Id);
+            return req.CreateResponse(HttpStatusCode.OK);
+            //return new OkObjectResult("Welcome to Azure Functions!");
+          }
+          catch (Exception ex) {
+            _logger.LogError(ex, "Whooooooops");
+            return req.CreateResponse(HttpStatusCode.InternalServerError);
+          }
         }
     }
 }
